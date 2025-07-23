affbd24799b8babe022599edebb17d4c5034fc46
vladislavafed <feder_v_s@mail.ru>
2025-07-23 16:21:36 +0700

    Changed input data - added outliers, added cumulative sum by month, added graphs

--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ from numpy.ma.extras import unique
 pd.set_option("display.max_columns", None)
 pd.set_option('display.width', None)
 
-data = pd.read_csv("dales_data.csv")
+data = pd.read_csv("order_data.csv")
 
 #Converting the column to datetime
 data['order_date'] = pd.to_datetime(data['order_date'])
@@ -16,10 +16,10 @@ data['amount'] = data['quantity'] * data['price']
 
 
 #total revenue - общая выручка
-total_rev = sum(data['amount'])
+total_rev = round(sum(data['amount']), 2)
 
 #average receipt
-average_receipt = total_rev / len(data)
+average_receipt = round(total_rev / len(data), 2)
 
 #number of orders per day
 orders_day = data.groupby('order_date')['order_id'].count().reset_index()
@@ -31,14 +31,19 @@ unique_customers = unique(data['customer_id'])
 #cumulative revenue
 cum_rev = data['amount'].cumsum()
 
+#cumulative revenue by month
+data['ym'] = pd.to_datetime(data['order_date']).dt.strftime('%Y-%m')
+data_cumsum = data.groupby('ym')['amount'].agg({'sum'}).reset_index()
+
 #top 5 products by sales
 prod = data.groupby('product')['amount'].sum().sort_values(ascending=False).reset_index()
 top5 = prod['product'].head(5).tolist()
 
-#print(data)
-print(f"Total revenue for the last 5 months - {total_rev}\n")
+print(data)
+print(f"Total revenue for the last 6 months - {total_rev}\n")
 print(f"Average receipt for each purchase - {average_receipt}\n")
 print(f"Number of orders per day - {orders_day}\n")
 print(f"Unique customers - {unique_customers}\n")
-print(f"Сumulative revenue for the last 5 months - {cum_rev}\n")
+print(f"Сumulative revenue for the last 6 months - {cum_rev}\n")
 print(f"Top 5 products by sales - {top5[0]}, {top5[1]}, {top5[2]}, {top5[3]}, {top5[4]}\n")
+
--- store_details.py
+++ store_details.py
@@ -3,7 +3,7 @@ import random
 from datetime import datetime, timedelta
 
 #number of orders
-num_orders = 5000
+num_orders = 8000
 
 #list of products and their prices
 products = {
@@ -18,26 +18,45 @@ products = {
 #list of products
 products_name = list(products.keys())
 
-#Generate random dates (for the last 150 days)
-start_date = datetime.today() - timedelta(days=150)
-dates = [start_date + timedelta(days=random.randint(0, 149)) for _ in range(num_orders)]
+#Generate random dates (for the last 180 days)
+start_date = datetime.today() - timedelta(days=180)
+dates = [start_date + timedelta(days=random.randint(0, 179)) for _ in range(num_orders)]
 
 #Orders generation
 data = {
     "order_id": [800 + i for i in range(num_orders)],
-    "customer_id": [f"P{random.randint(1, 400):03}" for i in range(num_orders)],
+    "customer_id": [f"P{random.randint(1, 600):03}" for i in range(num_orders)],
     "order_date": dates,
     "product": [random.choice(products_name) for i in range(num_orders)],
-    "quantity": [random.randint(1, 4) for i in range(num_orders)],
+    "quantity": [],
+    "price": []
 }
+# Генерация quantity и price с выбросами
+for i in range(num_orders):
+    product = data["product"][i]
+    base_price = products[product]
+
+    # 8% заказов — выбросы
+    if random.random() < 0.08:
+        # Выброс в количестве (заказали много штук)
+        quantity = random.randint(10, 50)
+
+        # Выброс в цене (например, неверная цена или VIP-продажа)
+        price = base_price * random.uniform(2.0, 4.0)
+    else:
+        quantity = random.randint(1, 5)
+        price = base_price * random.uniform(0.95, 1.05)
+
+    data["quantity"].append(quantity)
+    data["price"].append(round(price, 2))
 
 #Add price
-data["price"] = [products[i] for i in data["product"]]
+#data["price"] = [products[i] for i in data["product"]]
 
 #To create DataFrame
 df = pd.DataFrame(data)
 
-df.to_csv("dales_data.csv", index=False)
+df.to_csv("order_data.csv", index=False)
 
 
 print(df)
